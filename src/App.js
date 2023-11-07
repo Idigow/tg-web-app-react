dea3fa59706b32d31211b8e79b7dcdddf1c8e25a
Muhammad Idigov <idmag95@gmail.com>
2023-11-07 08:02:00 +0300

    Fix App

--- src/App.js
+++ src/App.js
@@ -7,14 +7,12 @@ function App() {
   useEffect(() => {
     tg.ready()
   },[])
-  const onClose = () => {
-    tg.close();
-  }
+  
 
   return (
     <div className="App">
       work
-      <button onClick={onClose}>Закрыть</button>
+      <button>Work</button>
     </div>
   );
 }
