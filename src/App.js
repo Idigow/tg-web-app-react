ab254bef463255c53a07bdaba7c17efdd3627ffc
Muhammad Idigov <idmag95@gmail.com>
2023-11-08 07:24:03 +0300

    forth commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import { useEffect } from 'react';
 import './App.css';
+import { useTelegram } from './components/hooks/useTelegram';
+import Header from './components/Header/Header';
 
-const tg = window.Telegram.WebApp;
 function App() {
-
     const { tg, onToggleButton } = useTelegram;
 
-
   useEffect(() => {
-    tg.ready()
-  },[])
+    tg.ready();
+  }, [tg])
   
 
   return (
     <div className="App">
+      <Header/>
       <button onClick={onToggleButton}>Toggle</button>
     </div>
   );
